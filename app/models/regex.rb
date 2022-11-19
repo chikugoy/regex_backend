f67e5321a2c87da22734069d4d4a218e4c5cf371
chikugoy <chiku7101@gmail.com>
2022-11-19 11:51:02 +0900

    add language

--- app/models/regex.rb
+++ app/models/regex.rb
@@ -4,6 +4,8 @@ class Regex < Firestore::FirestoreRecord
   def self.create(data)
     raise Exception.new("#{self.class.name} create args not hash") unless data.is_a?(Hash)
 
+    data[:language] ||= ENV['REGX_LANGUAGE']
+    data[:language_version] ||= ENV['REGX_LANGUAGE_VERSION']
     data[:created_at] ||= Time.current
     data[:updated_at] ||= Time.current
 
