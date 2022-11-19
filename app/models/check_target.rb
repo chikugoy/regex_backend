8a2abc6f91df86317a9079aad2a9c66c1539b0af
chikugoy <chiku7101@gmail.com>
2022-11-19 12:04:14 +0900

    refactor remove

--- app/models/check_target.rb
+++
@@ -1,7 +0,0 @@
-class CheckTarget
-  include ActiveModel::Model
-  include ActiveModel::Attributes
-
-  attribute :target, :string
-  attribute :result, :check_target_result
-end
--- app/models/type_check_target_result.rb
+++
@@ -1,9 +0,0 @@
-require 'active_model'
-
-class TypeCheckTargetResult < ActiveModel::Type::Value
-  def cast_value(value)
-    CheckTargetResult.new(value)
-  end
-end
-
-ActiveModel::Type.register(:check_target_result, TypeCheckTargetResult)
