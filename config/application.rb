dbc50d37bd7b5ca165a0e67bbd89598382e1179f
chikugo <chiku7101@gmail.com>
2022-11-24 18:08:49 +0900

    add enable_dependency_loading add !app/lib/firebase/auth.rb

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Myapp
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.yml').to_s]
 
+    config.enable_dependency_loading = true
     config.autoload_paths << "#{root}/lib"
   end
 end
